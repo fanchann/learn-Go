48c9f4ef08181fc68b555fcb555dbcd4c713e6d2
fannchann <fannyarifnass@gmail.com>
2022-09-19 21:25:49 +0700

    updated

--- 17-forOfLoop.go
+++ 17-forOfLoop.go
@@ -8,7 +8,31 @@ func main() {
 	// 	counter++
 	// }
 	//short
-	for counter := 1; counter <= 10; counter++ {
-		fmt.Println("Ini angka ke", counter)
+	// for counter := 1; counter <= 10; counter++ {
+	// 	fmt.Println("Ini angka ke", counter)
+	// }
+
+	// jika untuk mengakses tipe data slice
+
+	slice := []string{"Fanny", "Arif", "Nasrudin"}
+	for i := 0; i < len(slice); i++ {
+		fmt.Println(slice[i])
+	}
+
+	//juga bisa memakai
+
+	for index, name := range slice {
+		fmt.Println("index", index, "=", name)
+	}
+
+	//jika data bertipe map gunakan ini
+
+	person := map[string]string{
+		"name":  "Fanny",
+		"title": "Programmer",
+	}
+
+	for key, value := range person {
+		fmt.Println(key, "=", value)
 	}
 }
